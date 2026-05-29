d6b85819eb7d23da9363b9e7b4681ea7f91258f8
Protap Biswas <protapb23@gmail.com>
2026-05-29 12:50:31 +0600

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -19,7 +19,7 @@ const ContactUs = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
                                 </svg>
-                                <span>Kumkumari,Ashulia,Dhaka</span>
+                                <span>Kumkumari,Ashulia,Dhaka,Bangladesh</span>
                             </p>
                             <p className="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
